846dfda4dd68545db04fe1ef8891b0852872e341
Florian Anderiasch <fa@art-core.org>
2012-03-24 02:33:29 +0100

    Add cinder support

--- src/jonase/kibit/reporters.clj
+++ src/jonase/kibit/reporters.clj
@@ -25,3 +25,14 @@
       (pprint-code expr)
       (newline))))
 
+;; This reporter writes Cinder's XML format
+(defn xml-reporter [check-map]
+  (let [{:keys [line expr alt]} check-map]
+    (do
+      (printf "  <error line=\"%s\" column=\"0\" severity=\"warning\"" line)
+      (print " message=\"")
+      (pprint-code alt)
+      (printf "\" pattern=\"")
+      (pprint-code expr)
+      (print "\" />")
+      (newline))))
---
+++ src/leiningen/kibit_xml.clj
@@ -0,0 +1,30 @@
+(ns leiningen.kibit_xml
+  (:require [clojure.tools.namespace :as clj-ns]
+            [clojure.java.io :as io]
+            [jonase.kibit.core :as kibit]
+            [jonase.kibit.reporters :as reporters]))
+
+(defn- convert-to-path
+  "Converts namespace representation to file name"
+  [path]
+  (str "src/" (clojure.string/replace (str path) "." "/") ".clj"))
+
+(defn kibit_xml
+  "Make kibit's findings available to Eclipse/CCW using the Cinder plugin"
+  [project]
+  (let [paths (or (:source-paths project) [(:source-path project)])
+        source-files (mapcat #(-> % io/file clj-ns/find-clojure-sources-in-dir)
+                             paths)]
+    (do
+      (println "<cruisecontrol>")
+      (printf "<padawan>\n")
+      (doseq [source-file source-files]
+        (printf "<file name=\"%s\">\n"
+                (convert-to-path
+                  (or (second (clj-ns/read-file-ns-decl source-file)) source-file)))
+        (kibit/check-file source-file :reporter reporters/xml-reporter)
+        (printf "</file>")
+        (newline)
+        (flush))
+      (println "</padawan>")
+      (println "</cruisecontrol>"))))
